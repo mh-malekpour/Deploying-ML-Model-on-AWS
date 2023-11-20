26cb12e11aa8b70cf16ad43df25d5fb74c0078fe
Mohammadhossein Malekpour <mohammadhossein.malekpour@gmail.com>
2023-11-20 16:51:35 -0500

    add final endpoint response to the orchestrator output using shared dict

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -3,49 +3,35 @@ import requests
 import threading
 import json
 import time
+import uuid
 
-def load_json(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-def save_json(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
-
-instance_details_data = load_json('../infrastructure/instance_details.json')
-workers_data = load_json('workers.json')
-
-for i, worker in enumerate(workers_data):
-    dns = instance_details_data[i%4]['PublicDNS']
-    container_i = f"container{i+1}"
-    workers_data[container_i]['ip'] = dns
-# Save the updated data back to workers.json
-save_json(workers_data, 'workers.json')
-print("Workers json file has been updated with infra PublicDNSs.")
-
-
-app= Flask(__name__)
+app = Flask(__name__)
 lock = threading.Lock()
 request_queue = []
+responses = {}  # Dictionary to store responses
 
-def send_request_to_container(container_id, container_info):
-    # code to call instance, should get the ip, port, send the request to it
-    ip_address =container_info['ip']
+def send_request_to_container(container_id, container_info, request_id):
+    # Code to call instance, get the IP and port, send the request to it
+    ip_address = container_info['ip']
     port = container_info['port']
     url = f"http://{ip_address}:{port}/run_model"
     print(url)
     try:
-        # Send an HTTP GET request to the URL
         response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
             print("Request was successful.")
             print("Response content:")
             print(response.text)
+            with lock:
+                responses[request_id] = response.text  # Store the response
         else:
             print(f"Request failed with status code: {response.status_code}")
+            with lock:
+                responses[request_id] = f"Request failed with status code: {response.status_code}"
     except requests.exceptions.RequestException as e:
         print(f"Request failed with an error: {e}")
+        with lock:
+            responses[request_id] = f"Request failed with an error: {e}"
 
 def update_container_status(container_id, status):
     with lock:
@@ -55,8 +41,7 @@ def update_container_status(container_id, status):
         with open('workers.json', 'w') as f:
             json.dump(data, f)
 
-
-def process_request():
+def process_request(request_id):
     with lock:
         with open('workers.json', 'r') as f:
             data = json.load(f)
@@ -67,17 +52,23 @@ def process_request():
             break
     if free_container:
         update_container_status(free_container, 'busy')
-        send_request_to_container(free_container, data[free_container])
+        send_request_to_container(free_container, data[free_container], request_id)
         update_container_status(free_container, 'free')
     else:
         request_queue.append("incoming_request_data")
 
-
-@app.route('/mew_request', methods=['GET'])
+@app.route('/new_request', methods=['GET'])
 def new_request():
-    threading.Thread(target=process_request, args=()).start()
-    return jsonify({"message": "Request received and processing started."})
+    request_id = str(uuid.uuid4())  # Generate a unique request ID
+    threading.Thread(target=process_request, args=(request_id,)).start()
 
+    # Wait for the response to be available
+    while request_id not in responses:
+        time.sleep(0.1)
+
+    # Retrieve and return the response
+    response = responses.pop(request_id)  # Remove the response from the dictionary
+    return jsonify({"message": "Request processed.", "response": response})
 
 if __name__ == "__main__":
-    app.run(port=80)
+    app.run(host='0.0.0.0', port=80)
